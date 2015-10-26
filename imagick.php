ca9a25b43166014d93c248b24a30111191a58ef2
iksi <jur@iksi.cc>
2015-10-26 23:43:56 +0100

    Create imagick.php

---
+++ imagick.php
@@ -0,0 +1,53 @@
+<?php
+
+thumb::$drivers['imagick'] = function($thumb) {
+    try {
+        $img = new Imagick($thumb->root());
+
+        if ($img->getImageFormat() && $img->getNumberImages()) {
+            // Animated gif
+            $img = $img->coalesceImages();
+
+            foreach ($img as $frame) {
+                // $frame->cropImage($crop_w, $crop_h, $crop_x, $crop_y); 
+                // $frame->thumbnailImage($size_w, $size_h); 
+                $dimensions = clone $thumb->source->dimensions();
+                $dimensions->fitWidthAndHeight($thumb->options['width'], $thumb->options['height'], $thumb->options['upscale']);
+                $frame->thumbnailImage($dimensions->width(), $dimensions->height(), true);
+                // $frame->setImagePage($size_w, $size_h, 0, 0); 
+            }
+
+            $img = $img->optimizeImageLayers();
+            $img = $img->deconstructImages(); 
+            $img->writeImages($thumb->destination->root, true); 
+
+        } else {
+            // Other image
+            if($thumb->options['crop']) {
+                $img->cropThumbnailImage($thumb->options['width'], $thumb->options['height']);
+            } else {
+                $dimensions = clone $thumb->source->dimensions();
+                $dimensions->fitWidthAndHeight($thumb->options['width'], $thumb->options['height'], $thumb->options['upscale']);
+                @$img->thumbnailImage($dimensions->width(), $dimensions->height());
+            }
+
+            if($thumb->options['grayscale']) {
+                $img->modulateImage(100, 0, 100); 
+            }
+
+            if($thumb->options['blur']) {
+                $img->gaussianBlurImage($thumb->options['blurpx']);
+            }
+
+            if($thumb->options['autoOrient']) {
+            }   
+
+            $img->setImageCompressionQuality( (int) $thumb->options['quality']);
+            $img->stripImage();
+
+            @$img->writeImage($thumb->destination->root);
+        }
+    } catch(Exception $e) {
+        $thumb->error = $e;
+    }
+};
